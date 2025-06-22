1b145e3d33a0162397b9539c4a44993ff3032f6b
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2025-06-22 17:08:21 -0600

    chore: Remove Firebase configuration files and dependencies to streamline project setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:icheja_mobile/core/application/dependency_injection.dart';
 import 'package:icheja_mobile/core/router/app_router.dart';
 import 'package:icheja_mobile/common/presentation/theme/theme.dart';
-import 'package:icheja_mobile/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await setupDependencies();
   runApp(const MyApp());
 }
