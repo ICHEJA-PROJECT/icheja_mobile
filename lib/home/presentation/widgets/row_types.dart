5310b2249749b0b63bd23bccf5a2d9a9d5b9bf9f
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2025-07-02 16:48:37 -0600

    feat: Add CustomNetworkImage widget for improved image handling in RowTypes

--- lib/home/presentation/widgets/row_types.dart
+++ lib/home/presentation/widgets/row_types.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:icheja_mobile/common/presentation/theme/color_theme.dart';
+import 'package:icheja_mobile/common/presentation/widgets/custom_network_image.dart';
 
 class RowTypes extends StatelessWidget {
   final String text;
@@ -56,12 +57,12 @@ class RowTypes extends StatelessWidget {
           const SizedBox(width: 18.0),
           Expanded(
             flex: 3,
-            child: Image.network(
-              imageUrl,
+            child: CustomNetworkImage(
+              imageUrl: imageUrl,
+              placeHolderImage: const CircularProgressIndicator(),
+              errorImage: const Icon(Icons.error, size: 50.0),
               width: 50.0,
               height: 50.0,
-              fit: BoxFit.contain,
-              matchTextDirection: true,
             ),
           ),
         ],
